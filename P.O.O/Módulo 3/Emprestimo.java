604275865541e7f6f47cfa4668e842263a1a30e6
Danilo César <dkdanjolly@gmail.com>
2025-11-18 14:45:46 -0300

    Create Emprestimo.java

---
+++ P.O.O/Módulo 3/Emprestimo.java
@@ -0,0 +1,7 @@
+Date dataRetirada = new Date();
+
+        Date dataDevolucao = new Date();
+
+        Livro livro = new Livro("Java Basics", new Autor("Alan Turing", "Inglês"), "Tecnologia", true);
+
+        Usuario usuario = new Usuario("Gabriel", 21);
