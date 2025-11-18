ab27cafa0cfdc99728e48fb0cb5f5f4e0952a922
Danilo César <dkdanjolly@gmail.com>
2025-11-18 14:46:28 -0300

    Add LivroTest.java file

---
+++ P.O.O/Módulo 3/LivroTest.java
@@ -0,0 +1,6 @@
+ Autor autor = new Autor("Jess", "Brasileira");
+
+        Livro livro1 = new Livro("Java Basico", autor, "tecnologia", true);
+
+        Livro livro2 = new Livro("Java Avançado", autor, "tecnologia", false);
+
