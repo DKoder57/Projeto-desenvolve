55e7327e8b3df79b4bbedd048e2bcc2149b8b288
Danilo César <dkdanjolly@gmail.com>
2025-11-25 16:31:19 -0300

    Remove unused date variables
    Removed unused date variables from EmprestimoTest.java

--- P.O.O/Módulo 3/EmprestimoTest.java
+++ P.O.O/Módulo 3/EmprestimoTest.java
@@ -1,7 +1,3 @@
-Date dataRetirada = new Date();
-
-        Date dataDevolucao = new Date();
-
         Livro livro = new Livro("Java Basics", new Autor("Alan Turing", "Inglês"), "Tecnologia", true);
 
         Usuario usuario = new Usuario("Gabriel", 21);
